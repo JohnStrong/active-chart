ae90cdfa21d65aa53a5a0dea7fe16f8946a2efd4
John Strong <strngj411@gmail.com>
2014-06-10 12:29:13 +0100

    another boring api change

--- active-chart.js
+++ active-chart.js
@@ -5,8 +5,28 @@
 ;( function(d3) {
 
 	'use strict';
-	
-	var _activeChart = ( function() {
+
+	var _error = {
+		'invalidId': 'first function parameter much be a valid element id',
+		'invalidOrient': 'orient property much be set to either veritical or horizontal'
+	},
+
+	_consts = {
+		'width': 1,
+		'innerPadding': 0.5,
+		'outerPadding': 0,
+		'orient': 'vertical'
+	},
+
+	_util = {
+
+		is: function(entity, type) {
+			var clas = Object.prototype.toString.call(entity).slice(8, -1);
+			return (clas === type);
+		}
+	},
+
+	_activeChart = ( function() {
 
 			var _parentWidth = function() {
 				return d3.select(this.parentNode).style('width');
@@ -37,97 +57,72 @@
 				// set props for chart...
 			};
 
-	} )(),
-	
-	activeChart = ( function() {
-		
-		var base = {};
+	} )();
 
-		base._error = {
-			'invalidId': 'first function parameter much be a valid element id',
-			'invalidOrient': 'orient property much be set to either veritical or horizontal'
-		};
-
-		base._consts = {
-			'width': 1,
-			'innerPadding': 0.5,
-			'outerPadding': 0,
-			'orient': 'vertical'
-		};
-
-		base._util = {
-
-			is: function(entity, type) {
-				var clas = Object.prototype.toString.call(entity).slice(8, -1);
-				return (clas === type);
-			}
+	// Active Chart api
+	function ActiveChart(id) {
+		try {
+			this.node = d3.select(id);
 		}
+		catch (e) {
+			throw new Error(_error.invalidId);
+		}
+	}
 
-		var self = base;
-
-		// node id
-		self['node'] = function(id) {
-			try {
-				this.node = d3.select(id);
-			}
-			catch (e) {
-				throw new Error(this._error.invalidId);
-			}
-
-			return this;
-		};
-
-		// data to chart
-		self['data'] = function(data) {
+	ActiveChart.prototype = {
+		
+		'data': function(data) {
 			this.points = data;
 
 			return this;
-		};
+		},
 
 		// width scale (1 -> 100%, .5 -> 50%)
-		self['scale'] = function(widthScale) {
+		'scale': function(widthScale) {
 			this.scale = widthScale;
 
 			return this;
-		};
+		},
 
 		// inner & outer chart padding
-		self['padding'] = function(padding) {
+		'padding': function(padding) {
 
 			this.padding = {};
 
-			this.padding.inner = padding[0]? padding[0] : this._consts.innerPadding,
-			this.padding.outer = padding[1]? padding[1] : this._consts.outerPadding;
+			this.padding.inner = padding[0]? padding[0] : _consts.innerPadding,
+			this.padding.outer = padding[1]? padding[1] : _consts.outerPadding;
 
 			return this;
-		};
+		},
 
 		// veritical or horizontal
-		self['orient'] = function(orient) {
+		'orient': function(orient) {
 
-			var isOrientString = this._util.is(orient, 'String');
+			var isOrientString = _util.is(orient, 'String');
 
 			if(isOrientString) {
 				if(orient === 'vertical' || orient === 'horizontal') {
 					this.orient = orient;
 				} else {
-					this.orient = this._consts.orient;
+					this.orient = _consts.orient;
 				}
 			} else {
-				throw new Error(this._error.invalidOrient);
+				throw new Error(_error.invalidOrient);
 			}
 
 			return this;
-		};
+		},
 
 		// draw the chart
-		self['draw'] = function() {
+		'draw': function() {
 			_activeChart.call(this);
-		};
+		}
+	};
 
-		return self;
-
-	} )();
+	// removes the need for user to use 'new'	
+	var activeChart = function(id) {
+		return new ActiveChart(id);
+	};
 
 	window.activeChart = activeChart;
 
